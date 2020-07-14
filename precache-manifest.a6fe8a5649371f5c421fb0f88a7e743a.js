5b2fba96364b069e9a539ab8d89974ee980d4ea7
Robin Lin <robinrobin19980202@gmail.com>
2020-07-14 18:07:55 +0800

    Updates

---
+++ precache-manifest.a6fe8a5649371f5c421fb0f88a7e743a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68b6465cd78b5a83864f978f2187540",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec7e2a1851736179ff72",
+    "url": "/monsters-rolodex/static/css/main.13ff8cc3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7e2a1851736179ff72",
+    "url": "/monsters-rolodex/static/js/main.227fc8e2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
